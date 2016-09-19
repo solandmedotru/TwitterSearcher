8fe0d1db1016d35a690b0f5641e3a16f9288d87a
Solomin Andrey <solomin.andrey@gmail.com>
2016-09-19 22:00:37 +0300

    created MainActivity

--- app/src/main/java/ru/solandme/twittersearcher/MainActivity.java
+++ app/src/main/java/ru/solandme/twittersearcher/MainActivity.java
@@ -1,15 +1,38 @@
 package ru.solandme.twittersearcher;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SEARCHES = "searches";
+    private EditText queryEditText;
+    private EditText tagEditText;
+    private FloatingActionButton saveFloatingActionButton;
+    private SharedPreferences savedSearches;
+    private List<String> tags;
+    private SearchesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +41,156 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        queryEditText = ((TextInputLayout) findViewById(R.id.queryTextInputLayout)).getEditText();
+        queryEditText.addTextChangedListener(textWatcher);
+        tagEditText = ((TextInputLayout) findViewById(R.id.tagTextInputLayout)).getEditText();
+        tagEditText.addTextChangedListener(textWatcher);
+
+        savedSearches = getSharedPreferences(SEARCHES, MODE_PRIVATE);
+
+        tags = new ArrayList<>(savedSearches.getAll().keySet());
+        Collections.sort(tags, String.CASE_INSENSITIVE_ORDER);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new SearchesAdapter(tags, itemClickListener, itemLongClickListener);
+        recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new ItemDivider(this));
+
+        saveFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        saveFloatingActionButton.setOnClickListener(saveButtonListener);
+        updateSaveFAB();
+    }
+
+    private final TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            updateSaveFAB();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
+    private void updateSaveFAB() {
+        if (queryEditText.getText().toString().isEmpty() ||
+                tagEditText.getText().toString().isEmpty())
+            saveFloatingActionButton.hide();
+        else
+            saveFloatingActionButton.show();
+    }
+
+    private final View.OnClickListener saveButtonListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String query = queryEditText.getText().toString();
+                    String tag = tagEditText.getText().toString();
+
+                    if (!query.isEmpty() && !tag.isEmpty()) {
+                        ((InputMethodManager) getSystemService(
+                                Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                                view.getWindowToken(), 0);
+
+                        addTaggedSearch(tag, query);
+                        queryEditText.setText("");
+                        tagEditText.setText("");
+                        queryEditText.requestFocus();
+                    }
+                }
+            };
+
+    private void addTaggedSearch(String tag, String query) {
+        SharedPreferences.Editor preferencesEditor = savedSearches.edit();
+        preferencesEditor.putString(tag, query);
+        preferencesEditor.apply();
+
+        if (!tags.contains(tag)) {
+            tags.add(tag);
+            Collections.sort(tags, String.CASE_INSENSITIVE_ORDER);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    private final View.OnClickListener itemClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String tag = ((TextView) view).getText().toString();
+            String urlString = getString(R.string.search_URL) +
+                    Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlString));
+
+            startActivity(webIntent);
+        }
+    };
+
+    private final View.OnLongClickListener itemLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+            final String tag = ((TextView) view).getText().toString();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(getString(R.string.share_edit_delete_title, tag));
+            builder.setItems(R.array.dialog_items, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case 0:
+                                    shareSearch(tag);
+                                    break;
+                                case 1:
+                                    tagEditText.setText(tag);
+                                    queryEditText.setText(savedSearches.getString(tag, ""));
+                                    break;
+                                case 2:
+                                    deleteSearch(tag);
+                                    break;
+                            }
+                        }
+                    }
+            );
+
+            builder.setNegativeButton(getString(R.string.cancel), null);
+            builder.create().show();
+            return true;
+        }
+    };
+
+    private void shareSearch(String tag) {
+        String urlString = getString(R.string.search_URL) + Uri.encode(savedSearches.getString(tag, ""), "UTF-8");
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_message, urlString));
+        shareIntent.setType("text/plain");
+
+        startActivity(Intent.createChooser(shareIntent,
+                getString(R.string.share_search)));
+    }
+
+    private void deleteSearch(final String tag) {
+        AlertDialog.Builder confirmBuilder = new AlertDialog.Builder(this);
+        confirmBuilder.setMessage(getString(R.string.confirm_message, tag));
+        confirmBuilder.setNegativeButton(getString(R.string.cancel), null);
+        confirmBuilder.setPositiveButton(getString(R.string.delete),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        tags.remove(tag);
+                        SharedPreferences.Editor preferencesEditor = savedSearches.edit();
+                        preferencesEditor.remove(tag);
+                        preferencesEditor.apply();
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+        );
+        confirmBuilder.create().show();
     }
 }
